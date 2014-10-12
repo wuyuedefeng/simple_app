4ee36c9764a4abb4674c35677435feca2ce89cbe
senwang <senwang@senwangdeMacBook-Pro.local>
2014-10-12 17:26:15 +0800

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "HomePage" do
+    it "should have the content 'Simple App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Simple App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Simple App | Home")
+    end
+  end
+
+  describe "HelpPage" do
+    it "should have the content 'help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('help')
+    end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Simple App | Help")
+    end
+  end
+
+  describe "AboutPage" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Simple App | About Us')
+    end
+  end
+end
