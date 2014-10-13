eb074b2bc25adc001d08ee5c2d71e49f637291e2
senwang <578595193@qq.com>
2014-10-13 19:55:57 +0800

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'rails'
-
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sprockets', '2.11.0'
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,43 +1,35 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "HomePage" do
-    it "should have the content 'Simple App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Simple App')
-    end
+describe "Static pages" do
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Simple App")
-    end
+  subject { page }
 
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('Simple App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
-  describe "HelpPage" do
-    it "should have the content 'help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('help')
-    end
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Simple App | Help")
-    end
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
-  describe "AboutPage" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+  describe "About page" do
+    before { visit about_path }
 
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Simple App | About Us')
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
